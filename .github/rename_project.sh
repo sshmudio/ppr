5afa6b610af55b5ebbdefe91d3e6047cb075bb85
sshmudio <sshmudio@users.noreply.github.com>
2022-07-08 10:26:52 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="sshmudio"
+original_name="ppr"
+original_urlname="ppr"
+original_description="Awesome ppr created by sshmudio"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv ppr $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
